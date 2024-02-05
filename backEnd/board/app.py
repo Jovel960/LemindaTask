8c3ca7256b67a53f3161705d9e43d1bde1b4d6e5
Jovel960 <jovel960@icloud.com>
2024-02-05 13:07:22 +0200

    option to import app

--- backEnd/board/app.py
+++ backEnd/board/app.py
@@ -6,34 +6,35 @@ import db
 from utilities import (APP_SECRET_KEY, set_app_logger)
 
 def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = APP_SECRET_KEY
-    set_app_logger(app)
+    _app = Flask(__name__)
+    _app.config['SECRET_KEY'] = APP_SECRET_KEY
+    set_app_logger(_app)
     login_manager = LoginManager()
-    login_manager.init_app(app)
+    login_manager.init_app(_app)
     @login_manager.user_loader
     def load_user(user_id):
         user_data = db.swcdb.user.get_user(user_id)
         if user_data:
             return User(user_id=user_data['user_id'], user_name=user_data['user_name'], password=user_data['hashed_pwd'])
         return None
-    @app.before_request
+    @_app.before_request
     def log_request_info():
-        app.logger.info(f"Method: {request.method}")
+        _app.logger.info(f"Method: {request.method}")
         if request.method == "GET":
             print(request)
-            app.logger.info(f"Parameters: {request.args}")
+            _app.logger.info(f"Parameters: {request.args}")
         elif request.method in ["POST", "PUT", "PATCH"]:
             try:
-                app.logger.info(f"JSON data: {request.json}")
+                _app.logger.info(f"JSON data: {request.json}")
             except Exception as e:
-                app.logger.info("No JSON data")
-    bp.init(app)
-    @app.route('/', methods=['GET'])
+                _app.logger.info("No JSON data")
+    bp.init(_app)
+    @_app.route('/', methods=['GET'])
     def index():
         return 'Leminda Task'
-    return app
+    return _app
+
+app = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
